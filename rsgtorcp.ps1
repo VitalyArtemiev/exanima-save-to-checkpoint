ca3f50500cd52f74170216a4e70b086b3075928c
Виталий Артемьев <vitaly.artemiev@yandex.ru>
2022-01-22 02:50:56 +0300

    Added exception handling
    In case of any error, window won't close until after user acknowledges it.
    Probably unnecessary, but still.

--- rsgtorcp.ps1
+++ rsgtorcp.ps1
@@ -1,26 +1,30 @@
-Set-Location $env:APPDATA\Exanima
+try {
+	Set-Location $env:APPDATA\Exanima
 
-$latestrsg= Get-ChildItem -Attributes !Directory *.rsg | Sort-Object -Descending -Property LastWriteTime | select -First 1
+	$latestrsg= Get-ChildItem -Attributes !Directory *.rsg | Sort-Object -Descending -Property LastWriteTime | select -First 1
 
-if ($latestrsg -eq $null) {
-	echo "Could not find any RSG files"
-	exit
-} else {
-	echo "Latest rsg file is $latestrsg.Name"
+	if ($latestrsg -eq $null) {
+		throw "Could not find any RSG files"
+		exit
+	} else {
+		echo "Latest rsg file is $latestrsg.Name"
+	}
+
+	$goalrcpname= $latestrsg.Basename + '.rcp'
+	$backupname= $goalrcpname
+
+	$i = 2
+	while (Test-Path -Path $backupname) {
+		$backupname = $goalrcpname + "(" + ($i.ToString()) + ")"
+		$i++
+	}
+	if (Test-Path -Path $goalrcpname) {
+		echo "Renaming $goalrcpname to $backupname"
+		Rename-Item $goalrcpname $backupname
+	}
+
+	echo "Renaming $latestrsg to $goalrcpname"
+	Rename-Item $latestrsg $goalrcpname
+} catch {
+	read-host "Exception: $_; press enter to acknowledge"
 }
-
-$goalrcpname= $latestrsg.Basename + '.rcp'
-$backupname= $goalrcpname
-
-$i = 2
-while (Test-Path -Path $backupname) {
-	$backupname = $goalrcpname + "(" + ($i.ToString()) + ")"
-	$i++
-}
-if (Test-Path -Path $goalrcpname) {
-	echo "Renaming $goalrcpname to $backupname"
-	Rename-Item $goalrcpname $backupname
-}
-
-echo "Renaming $latestrsg to $goalrcpname"
-Rename-Item $latestrsg $goalrcpname
